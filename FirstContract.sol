bd1ed8a4e3822396932170ff93bfd1af470a0aa4
Zatezalo <zatezalo123@gmail.com>
2023-02-04 17:20:32 +0100

    First contract

---
+++ FirstContract.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.172 <0.9.0;
+
+contract Inbox {
+    string public message;
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
